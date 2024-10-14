9f85bf0b07a973a3a6075411f966bdbbbd892b54
wallace <wallaceguimaraes7@outlook.com>
2024-10-14 15:08:08 -0300

    Remove comments

--- WebApi/Authorization/AuthAttribute.cs
+++ WebApi/Authorization/AuthAttribute.cs
@@ -22,45 +22,6 @@ namespace WebApi.Authorization
 
         public override async Task OnActionExecutionAsync(ActionExecutingContext actionContext, ActionExecutionDelegate next)
         {
-            // var authOptions = actionContext.HttpContext.RequestServices.GetService<IOptions<AuthOptions>>();
-            // var dbContext = actionContext.HttpContext.RequestServices.GetService<ApiDbContext>();
-            var claims = actionContext.HttpContext.User.Claims;
-            var secret = actionContext.HttpContext.Request.Query["secret"].ToString();
-            // var userAuthorization = new UserAuthorization(dbContext);
-
-            // if (_requiredSecret || !string.IsNullOrEmpty(secret))
-            // {
-            //     if (secret == authOptions.Value.Secret)
-            //     {
-            //         await next();
-            //     }
-
-            //     actionContext.Result = new StatusCodeResult((int)HttpStatusCode.Unauthorized);
-            //     return;
-            // }
-
-
-
-
-            // await userAuthorization.FindUser(claims.UserId(), claims.Salt(), claims.HolderId());
-
-            // if (userAuthorization.UserNotFound || !userAuthorization.User.Active || userAuthorization.User.NeedChangePassword)
-            // {
-            //     actionContext.Result = new StatusCodeResult((int)HttpStatusCode.Unauthorized);
-            //     return;
-            // }
-
-            // var holderAsEmployee = claims.HolderAsEmployee();
-            // await userAuthorization.CheckPermission(actionContext.HttpContext.User.Claims.ApplicationId(), _scope, holderAsEmployee: holderAsEmployee, requiredSuperAdmin: _requiredSuperAdmin);
-
-            // if (userAuthorization.AccessGranted)
-            // {
-            //     await next();
-            // }
-            // else
-            // {
-            //     actionContext.Result = new StatusCodeResult((int)HttpStatusCode.Forbidden);
-            // }
         }
     }
 }
